4a2398af29b88ccf6f4850d08eaa157cb9c79f76
Juan Esteban  Morales Quevedo <proudman36@MacBook-Pro-de-Juan.local>
2023-02-16 15:57:32 -0500

    Tests

--- app_portfolio/tests.py
+++ app_portfolio/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Project
 
-# Create your tests here.
+
+class ProjectModelTest(TestCase):
+    def setUp(self):
+        self.project = Project.objects.create(
+            title="Test project",
+            description="This is a test project",
+            url="https://www.example.com"
+        )
+
+    def test_project_title(self):
+        self.assertEqual(self.project.title, "Test project")
+
+    def test_project_description(self):
+        self.assertEqual(self.project.description, "This is a test project")
+
+    def test_project_url(self):
+        self.assertEqual(self.project.url, "https://www.example.com")
+
+    def test_project_image(self):
+        self.assertIsNotNone(self.project.image)
